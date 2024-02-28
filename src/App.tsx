5c824fd7c4a07417dc43e8dfff5c7061a84fe6d9
aagbator <tonjoe99@gmail.com>
2024-02-28 09:59:05 +0100

    feat: add home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import {
-  Route,
-  Routes,
-  BrowserRouter
-} from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./hoc/Layout/Layout";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -12,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<></>} />
+            <Route index element={<Home></Home>} />
             <Route path="/euro-usd-details" element={<></>} />
             <Route path="/euro-gbp-details" element={<></>} />
             <Route path="*" element={<></>} />
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Breadcrump from "../../components/breadcrump/Breadcrump";
+
+export default function Home() {
+  return (
+    <div>
+      <Breadcrump title="Currency Exchanger" />
+    </div>
+  );
+}
