88cd71db55563bfa51bc36b83fd8c20913255491
aagbator <tonjoe99@gmail.com>
2024-02-28 19:23:10 +0100

    feat: added currency grid

---
+++ src/components/conversion-grid/ConversionGrid.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useConverter } from "../../contexts/Converter.context";
+
+type CurrencyRatesObject = {
+  currency: string;
+  rate: number;
+};
+
+const CurrencyGrid: React.FC = () => {
+  const { currencyRates } = useConverter();
+
+  const arrayOfCurrencyRates: CurrencyRatesObject[] = Object.entries(
+    currencyRates
+  ).map(([currency, rate]) => ({
+    currency,
+    rate,
+  }));
+
+  return (
+    <div className="currency-grid">
+      {arrayOfCurrencyRates.map(({ currency, rate }, i) => {
+        return (
+          <div
+            key={currency + i}
+            className="w-1/3 border-2 border-gray-200 rounded-lg bg-white p-5"
+          >
+            <h3 className="font-bold">{currency}</h3>
+            <p>{rate}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CurrencyGrid;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Breadcrump from "../../components/breadcrump/Breadcrump";
 import CurrencyConverter from "../../components/conversion-panel/ConversionPanel";
+import CurrencyGrid from "../../components/conversion-grid/ConversionGrid";
 
 export default function Home() {
   return (
     <div>
       <Breadcrump title="Currency Exchanger" />
       <CurrencyConverter />
+      <CurrencyGrid />
     </div>
   );
 }
